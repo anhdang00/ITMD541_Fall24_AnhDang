96a3b16d7c051dd5fc60e903f61a20240384fec1
anhdang00 <adang7@hawk.iit.edu>
2024-10-29 09:47:11 -0500

    change id

--- Lab3/js/script.js
+++ Lab3/js/script.js
@@ -1,9 +1,2 @@
-
+const wrapper = document.querySelector(".container");
 //get input from slider
-var slider = document.getElementById("tip-slider");
-var tipPercent = document.getElementById("tip-percent");
-
-tipPercent.innerHTML = slider.nodeValue; 
-slider.oninput = function(){
-    tipPercent.innerHTML = this.va
-}
