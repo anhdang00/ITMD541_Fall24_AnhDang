1553ccf5d1b35e0e248c46f897c3132982b856e5
anhdang00 <adang7@hawk.iit.edu>
2024-10-29 11:30:44 -0500

    calculate function

--- Lab3/js/script.js
+++ Lab3/js/script.js
@@ -5,6 +5,7 @@ var tipPercent = document.getElementById("tip-percent");
 tipPercent.innerHTML = slider.value;
 slider.oninput = function(){
     tipPercent.innerHTML = this.value;
+    calculateTip();
 }
 
 //take bill input
@@ -13,3 +14,12 @@ var billInput = document.getElementById("bill");
 //calculate and display tip and total
 var total = document.getElementById("total");
 var tipAmount = document.getElementById("converted-tip");
+
+function calculateTip(){
+    var bill = parseInt(billInput.value);
+    var tip = bill * (parseInt(slider.value)/100);
+    var totalAmount = bill + tip;
+
+    tipAmount.value = tip;
+    total.value = totalAmount;
+}
