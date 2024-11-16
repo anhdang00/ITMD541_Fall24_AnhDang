3aebd713de16c67a5f1d73189c872611ad77bda8
anhdang00 <adang7@hawk.iit.edu>
2024-11-15 22:58:13 -0600

    change 3

--- Lab4/anhdang00-lab4.js
+++ Lab4/anhdang00-lab4.js
@@ -7,9 +7,31 @@
     document.querySelector('.text-xl.mb-6.text-white.relative.z-20').innerHTML = "Leverage innovative strategies from Stellar Marketing to make your business <i><b>shine</b></i> and <i><b>succeed</b></i>.";
 })();
 //3. Change the image in the background of the hero 
+(function(){
+    const hero= document.getElementById("hero");
+    hero.style.backgroundImage = "url('https://picsum.photos/id/683/1280/720 ')";
+})();
 //4. Change the background color of the nav bar to the same color that is used in the footer.
+(function(){
+
+})();
 // 5. Remove the get started Call-to-action (CTA) from the hero.
+(function(){
+
+})();
 //6. In the services section change the icons color to a light blue (#6495ed).
+(function(){
+
+})();
 //7. Change the digital marketing icon to use ‘Ads Click’ instead of the current icon.
+(function(){
+
+})();
 //8. In the “Specialized Marketing Solutions” section make a change to the layout of the tiles
+(function(){
+
+})();
 //9. In the same section change the Musicians image
+(function(){
+
+})();
