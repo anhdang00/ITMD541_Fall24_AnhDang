b08da064dbba6fe2332ead51b8013a8e9a0ba50d
anhdang00 <adang7@hawk.iit.edu>
2024-11-16 00:06:27 -0600

    add spaces between requirements

--- Lab4/anhdang00-lab4.js
+++ Lab4/anhdang00-lab4.js
@@ -2,23 +2,28 @@
 (function(){
     document.querySelector('.text-4xl.font-bold.mb-4.text-white.relative.z-20').textContent="Supercharge Your Brand with Stellar Marketing";
 })();
+
 //2. Change the line of text below the hero headline 
 (function(){
     document.querySelector('.text-xl.mb-6.text-white.relative.z-20').innerHTML = "Leverage innovative strategies from Stellar Marketing to make your business <i><b>shine</b></i> and <i><b>succeed</b></i>.";
 })();
+
 //3. Change the image in the background of the hero 
 (function(){
     const hero= document.getElementById("hero");
     hero.style.backgroundImage = "url('https://picsum.photos/id/683/1280/720 ')";
 })();
+
 //4. Change the background color of the nav bar to the same color that is used in the footer.
 (function(){
     document.querySelector('.bg-blue-600.text-white.p-4').style.backgroundColor = "rgba(31, 41, 55,1)";
 })();
+
 // 5. Remove the get started Call-to-action (CTA) from the hero.
 (function(){
     document.querySelector('.relative.bg-blue-600.text-white.px-6.py-2.rounded').remove();
 })();
+
 //6. In the services section change the icons color to a light blue (#6495ed).
 (function(){
     const icons = document.querySelectorAll('.material-symbols-outlined.text-6xl');
@@ -27,15 +32,18 @@
         icon.style.color = "#6495ed";
     })
 })();
+
 //7. Change the digital marketing icon to use ‘Ads Click’ instead of the current icon.
 (function(){
     document.querySelector('.material-symbols-outlined.text-6xl').innerHTML='ads_click';
     console.log(document.querySelector('.material-symbols-outlined.text-6xl'));
 })();
+
 //8. In the “Specialized Marketing Solutions” section make a change to the layout of the tiles
 (function(){
 
 })();
+
 //9. In the same section change the Musicians image
 (function(){
 
