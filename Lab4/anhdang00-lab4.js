c1e53c1d9247683cef0c44fc040f9af3b28c92ab
anhdang00 <adang7@hawk.iit.edu>
2024-11-15 22:40:57 -0600

    change 1

---
+++ Lab4/anhdang00-lab4.js
@@ -0,0 +1,12 @@
+//1. In the hero section change the main headline 
+(function(){
+    document.querySelector('.text-4xl.font-bold.mb-4.text-white.relative.z-20').textContent="Supercharge Your Brand with Stellar Marketing";
+})();
+//2. Change the line of text below the hero headline 
+//3. Change the image in the background of the hero 
+//4. Change the background color of the nav bar to the same color that is used in the footer.
+// 5. Remove the get started Call-to-action (CTA) from the hero.
+//6. In the services section change the icons color to a light blue (#6495ed).
+//7. Change the digital marketing icon to use ‘Ads Click’ instead of the current icon.
+//8. In the “Specialized Marketing Solutions” section make a change to the layout of the tiles
+//9. In the same section change the Musicians image
