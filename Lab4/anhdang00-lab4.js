525a186e16de9e54d47ce092350483bddcd4a382
anhdang00 <adang7@hawk.iit.edu>
2024-11-15 22:50:35 -0600

    change 2

--- Lab4/anhdang00-lab4.js
+++ Lab4/anhdang00-lab4.js
@@ -3,6 +3,9 @@
     document.querySelector('.text-4xl.font-bold.mb-4.text-white.relative.z-20').textContent="Supercharge Your Brand with Stellar Marketing";
 })();
 //2. Change the line of text below the hero headline 
+(function(){
+    document.querySelector('.text-xl.mb-6.text-white.relative.z-20').innerHTML = "Leverage innovative strategies from Stellar Marketing to make your business <i><b>shine</b></i> and <i><b>succeed</b></i>.";
+})();
 //3. Change the image in the background of the hero 
 //4. Change the background color of the nav bar to the same color that is used in the footer.
 // 5. Remove the get started Call-to-action (CTA) from the hero.
